db8c4c94485f735be059820da356936cab49d1f2
Endiong Okop <55858951+flawed-homosapien@users.noreply.github.com>
2025-07-29 08:59:55 +0100

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
+  <?php
 <!doctype html>
 <html>
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title> Tester's Page</title>
+    <title> Testing Page</title>
   </head>
   <body>
-    <?php
-    header('Location: ./public/register_user.php');
+  
+    header('Location: my-php-project/public/register_user.php');
     exit;
-    ?>
+  
 </html>
